128cd947ec092cea55105e0d4e499ebfee0804a1
vanessaw1 <wang.vanessa1@gmail.com>
2016-06-02 09:26:25 -0500

    Update and rename CheckAnswerActionListener.java to GetSolutionActionListener.java

---
+++ src/GetSolutionActionListener.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class GetSolutionActionListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
