4da32de26b7ef6b488b27a5c862ae2b17739a26f
melissab17213 <melissab17213@isd273.org>
2016-06-02 22:20:17 -0500

    Update GetSolutionActionListener.java

--- src/GetSolutionActionListener.java
+++ src/GetSolutionActionListener.java
@@ -17,16 +17,29 @@ public class GetSolutionActionListener implements ActionListener {
 	
 	public void actionPerformed(ActionEvent e) {
 		String type = answerType.getText();
-		
-		if (type.equals("cos:")) {
-			answer.setText(Circle.cos[UnitCircleMain.num]);
-		} else if (type.equals("sin:")) {
-			answer.setText(Circle.sin[UnitCircleMain.num]);
-		} else if (type.equals("tan:")) {
-			answer.setText(Circle.tan[UnitCircleMain.num]);
-		} else {
-			answer.setText("Error in GetSolutionActionListener class");
-		}
+			
+		if (type.equals("cos:"))
+			answer.setText((Circle.cos[UnitCircleMain.num]));
+
+		else if (type.equals("sin:"))
+			answer.setText((Circle.sin[UnitCircleMain.num]));
+		else if (type.equals("tan:"))
+			answer.setText((Circle.tan[UnitCircleMain.num]));
+		else
+			answer.setText("ERROR...");
 	}
 
+	/*
+	 * TD:
+	 * after "get solution", make it so they can't do "check answer" cause that defeats the purpose
+	 * also if we add a counter of correct/incorrect, this will throw it off
+	 * 
+	 * and... work on layout, text font, text size, and IMAGES!
+	 */
+	
+	/*
+	 * when creating this, i also updated main and the randomnumbergenerator class
+	 */
 }
+
+
